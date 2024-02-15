7c885c00aa6f485b3fa4a7ff24626983c1da649b
Oleksii Mykhalchuk <omykhalchuk@engenious.io>
2024-02-15 14:45:22 +0100

    setup coordinator navigation

---
+++ GamesInfo/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  HomeCoordinator.swift
+//  GamesInfo
+//
+//  Created by Oleksii Mykhalchuk on 2/15/24.
+//
+
+import UIKit
+
+final class HomeCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    
+    var childCoordinator: [Coordinator] = []
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let mainController = MainViewController()
+        mainController.tabBarItem.image = UIImage(systemName: "house")
+        mainController.tabBarItem.title = NSLocalizedString("Home", comment: "Tabbar title")
+        mainController.title = NSLocalizedString("Home", comment: "Home View Title")
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.setViewControllers([mainController], animated: false)
+    }
+    
+    
+}
--- GamesInfo/Coordinator/OnboardingCoordinator.swift
+++ GamesInfo/Coordinator/OnboardingCoordinator.swift
@@ -5,11 +5,25 @@
 //  Created by Oleksii Mykhalchuk on 2/15/24.
 //
 
-import Foundation
+import UIKit
 
 final class OnboardingCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinator: [Coordinator] = []
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
 
     func start() {
-        //
+        showOnboarding()
+    }
+
+    private func showOnboarding() {
+        let controller = OnboardingViewController()
+        controller.title = NSLocalizedString("Welcome", comment: "Welcome Message")
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.setViewControllers([controller], animated: false)
     }
 }
