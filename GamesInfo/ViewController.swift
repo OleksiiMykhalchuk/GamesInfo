1c6d8aad1ee9e960b7e6b1e01de06d8f5cdab47e
Oleksii Mykhalchuk <omykhalchuk@engenious.io>
2024-02-15 09:14:31 +0100

    Initial Commit

---
+++ GamesInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamesInfo
+//
+//  Created by Oleksii Mykhalchuk on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
