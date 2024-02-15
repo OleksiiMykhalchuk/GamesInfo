760925fc8bf13fc7c8950d7559e2e44305daa3b5
Oleksii Mykhalchuk <omykhalchuk@engenious.io>
2024-02-15 10:53:23 +0100

    added AppLogger

--- GamesInfo/Utils/AppLogger.swift
+++ GamesInfo/Utils/AppLogger.swift
@@ -8,4 +8,61 @@
 import Foundation
 import OSLog
 
-final class AppLogger {}
+protocol Loggable {
+
+    func debug(_ message: String, file: String, function: String, line: Int)
+    func fault(_ message: String, file: String, function: String, line: Int)
+}
+
+extension Loggable {
+
+    func makeMessage(_ type: String, file: String, function: String, line: Int) -> String {
+        let file = file.components(separatedBy: "/").last
+        return "\(type) [\(file ?? "UNKNOWN")] [\(function)] [\(line)] -> "
+    }
+}
+
+final class AppLogger: Loggable {
+
+    enum LoggerType {
+        case debug
+        case fault
+
+        var message: String {
+            switch self {
+            case .debug:
+                return "🟢 [DEBUG]"
+            case .fault:
+                return "🔴 [FAULT]"
+            }
+        }
+    }
+
+    enum Category: String, Hashable {
+        case `default` = "Default"
+    }
+
+    private let logger: Logger
+
+    init?(_ category: Category = .default) {
+        guard let bundle = Bundle.main.bundleIdentifier else {
+            Logger().fault("\(Self.self) Failed to initialize")
+            return nil
+        }
+        logger = Logger(subsystem: bundle, category: category.rawValue)
+    }
+
+    func debug(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+        
+        let message = "\(makeMessage(LoggerType.debug.message, file: file, function: function, line: line)) \(message)"
+
+        logger.debug("\(message)")
+    }
+
+    func fault(_ message: String, file: String = #file, function: String = #function, line: Int = #line) {
+
+        let message = "\(makeMessage(LoggerType.debug.message, file: file, function: function, line: line)) \(message)"
+
+        logger.debug("\(message)")
+    }
+}
